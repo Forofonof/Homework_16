05210c76a33b3cb2d8de0055ec27277d26e5ab5c
Forofonof <DanielResult1@outlook.com>
2022-04-12 18:41:33 +0300

    Добавьте файлы проекта.

---
+++ Homework_16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        int randomNumber = random.Next(1,1024);
+        int degreeNumber = 2;
+        int degree = 1;
+        int number = degreeNumber;
+        bool isSearchActive = true;
+
+        while (isSearchActive == true)
+        {
+            number *= degreeNumber;
+            degree++;
+
+            if (number > randomNumber)
+            {
+                isSearchActive = false;
+            }
+        }
+        Console.WriteLine($"Случайное число: {randomNumber}\n{degreeNumber} в {degree} степени - {number}");
+    }
+}
